952ce66c3077c02c6f0c67106252807a4e29fa5c
Okoro Chidozie Joachim <okorochidozie9@gmail.com>
2026-07-02 11:02:21 -0700

    Rename App component to GetChurnShieldLanding

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Getchurnshieldlanding.JSX'
+import GetChurnShieldLanding from './Getchurnshieldlanding.JSX'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <GetChurnShieldLanding />
   </React.StrictMode>,
 )
