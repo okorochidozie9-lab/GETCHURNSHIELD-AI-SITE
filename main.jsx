131fc85de3ee98a5f0565811907475365d3cd0a5
Okoro Chidozie Joachim <okorochidozie9@gmail.com>
2026-07-02 11:05:59 -0700

    Replace GetChurnShieldLanding with App component

--- main.jsx
+++ main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import GetChurnShieldLanding from './Getchurnshieldlanding.JSX'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GetChurnShieldLanding />
+    <App />
   </React.StrictMode>,
 )
